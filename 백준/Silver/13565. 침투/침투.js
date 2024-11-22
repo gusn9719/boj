f2d9e0b94ec4227b1a2c87387db7413773e803c5
gumbi <150658637+gusn9719@users.noreply.github.com>
2024-11-22 22:36:38 +0900

    [Silver II] Title: 침투, Time: 352 ms, Memory: 92076 KB -BaekjoonHub

--- 백준/Silver/13565. 침투/침투.js
+++ 백준/Silver/13565. 침투/침투.js
@@ -4,17 +4,6 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 const [M, N] = input[0].split(' ').map(Number);
 const map = input.slice(1).map((line) => line.trim().split('').map(Number));
 
-const visited = Array.from({ length: M }, () => Array(N).fill(false));
-const queue = [];
-
-
-for (let x = 0; x < N; x++) {
-  if (map[0][x] === 0) {
-    queue.push([0, x]);
-    visited[0][x] = true;
-  }
-}
-
 const directions = [
   [-1, 0],
   [1, 0],
@@ -22,32 +11,37 @@ const directions = [
   [0, 1],
 ];
 
-let reachedInner = false;
+const visited = Array.from({ length: M }, () => Array(N).fill(false));
 
-while (queue.length > 0) {
-  const [y, x] = queue.shift();
+let result = false;
+
+const dfs = (x, y) => {
+  if (result) return;
 
   if (y === M - 1) {
-    reachedInner = true;
-    break;
+    result = true;
+    return;
   }
 
-  for (const [dy, dx] of directions) {
-    const ny = y + dy;
+  for (const [dx, dy] of directions) {
     const nx = x + dx;
+    const ny = y + dy;
 
-    if (
-      ny >= 0 &&
-      ny < M &&
-      nx >= 0 &&
-      nx < N &&
-      map[ny][nx] === 0 &&
-      !visited[ny][nx]
-    ) {
-      queue.push([ny, nx]);
+    if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+
+    if (map[ny][nx] === 0 && !visited[ny][nx]) {
       visited[ny][nx] = true;
+      dfs(nx, ny);
     }
+  }
+};
+
+for (let x = 0; x < N; x++) {
+  if (map[0][x] === 0 && !visited[0][x]) {
+    visited[0][x] = true;
+    dfs(x, 0);
+    if (result) break;
   }
 }
 
-console.log(reachedInner ? 'YES' : 'NO');
+console.log(result ? 'YES' : 'NO');
