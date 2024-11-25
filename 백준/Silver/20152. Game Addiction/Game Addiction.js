0163776f99972acaa2e546a83c963055c4b8eb20
gumbi <150658637+gusn9719@users.noreply.github.com>
2024-11-25 17:47:20 +0900

    [Silver II] Title: Game Addiction, Time: 96 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Silver/20152. Game Addiction/Game Addiction.js
+++ 백준/Silver/20152. Game Addiction/Game Addiction.js
@@ -8,19 +8,14 @@ const input = fs
 
 const [H, N] = input;
 
-const start = Math.min(H, N);
-const end = Math.max(H, N);
-
-const dp = Array.from({ length: end + 1 }, () => Array(end + 1).fill(0));
-
-dp[start][start] = 1;
-
-for (let y = start; y <= end; y++) {
-  for (let x = start; x <= end; x++) {
-    if (y < x) continue;
-    if (y > start) dp[y][x] += dp[y - 1][x];
-    if (x > start) dp[y][x] += dp[y][x - 1];
+function catalan(n) {
+  let result = BigInt(1);
+  for (let i = 0; i < n; i++) {
+    result = (result * BigInt(2 * n - i)) / BigInt(i + 1);
   }
+  return result / BigInt(n + 1);
 }
 
-console.log(dp[end][end]);
+const D = Math.abs(N - H);
+const numberOfPaths = catalan(D);
+console.log(numberOfPaths.toString());
