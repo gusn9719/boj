296be34f5e1f8e8e96aa97a83347cbd57efcf089
gumbi <150658637+gusn9719@users.noreply.github.com>
2024-11-25 20:21:57 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 77.04 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.js
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.js
@@ -1,11 +1,12 @@
 function solution(users, emoticons) {
+    var answer = []
     let maxSubscribers = 0;
     let maxRevenue = 0;
 
-    // 할인율 옵션
+   
     const discountRates = [10, 20, 30, 40];
 
-    // 모든 할인율 조합을 완전 탐색
+
     const dfs = (index, discounts) => {
         if (index === emoticons.length) {
             calculateResult(discounts);
@@ -49,7 +50,7 @@ function solution(users, emoticons) {
     };
 
     dfs(0, Array(emoticons.length).fill(0));
-    return [maxSubscribers, maxRevenue];
+    answer.push(maxSubscribers)
+    answer.push(maxRevenue)
+    return answer;
 }
-
-
