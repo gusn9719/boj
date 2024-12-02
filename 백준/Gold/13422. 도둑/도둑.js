b2e332d3fb2543aa8db58dfa83d597c6d45acb7d
gumbi <150658637+gusn9719@users.noreply.github.com>
2024-12-02 22:47:43 +0900

    [Gold IV] Title: 도둑, Time: 388 ms, Memory: 65532 KB -BaekjoonHub

--- 백준/Gold/13422. 도둑/도둑.js
+++ 백준/Gold/13422. 도둑/도둑.js
@@ -4,45 +4,43 @@ const input = fs.readFileSync('dev/stdin').toString().trim().split('\n');
 const T = Number(input[0]);
 let index = 1;
 
-for (let t = 0; t < T; t++) {
-    const [N, M, K] = input[index].split(' ').map(Number);
-    const houses = input[index + 1].split(' ').map(Number);
-    index += 2;
-
-    // 특별한 경우 처리: M이 N과 같을 경우
-    if (M === N) {
-        const totalSum = houses.reduce((acc, val) => acc + val, 0);
-        if (totalSum < K) {
-            console.log(1);
-        } else {
-            console.log(0);
-        }
-        continue;
-    }
-
-    // 슬라이딩 윈도우 계산
-    let count = 0;
-    let currentSum = 0;
-
-    // 처음 M개의 집에 대한 합을 계산
+const solve = function (N, M, K, houses) {
+    let ans = 0;
+    let currSum = 0;
+    
+    // 초기 M개의 집의 합 계산
     for (let i = 0; i < M; i++) {
-        currentSum += houses[i];
+        currSum += houses[i];
+    }
+    if (currSum < K) {
+        ans++;
     }
 
-    // 첫 번째 윈도우의 합이 K보다 작으면 카운트 증가
-    if (currentSum < K) {
-        count++;
-    }
-
-    // 슬라이딩 윈도우로 이동하며 나머지 부분 계산
-    for (let i = 1; i < N; i++) {
-        currentSum -= houses[(i - 1)];           // 이전 집 값을 빼고
-        currentSum += houses[(i + M - 1) % N];   // 새로 추가된 집 값을 더합니다
-
-        if (currentSum < K) {
-            count++;
+    // 슬라이딩 윈도우를 사용해 나머지 부분 계산 (원형 고려하지 않은 부분)
+    for (let i = 0; i < N - M; i++) {
+        currSum -= houses[i];
+        currSum += houses[i + M];
+        if (currSum < K) {
+            ans++;
         }
     }
 
-    console.log(count);
+    // 원형 슬라이딩 윈도우 처리 (마지막 부분)
+    if (N !== M) {
+        for (let i = N - M; i < N - 1; i++) {
+            currSum -= houses[i];
+            currSum += houses[i + M - N];
+            if (currSum < K) {
+                ans++;
+            }
+        }
+    }
+
+    return ans;
+}
+
+for (let i = 1; i < input.length; i += 2) {
+    const [N, M, K] = input[i].split(' ').map(Number);
+    const houses = input[i + 1].split(' ').map(Number);
+    console.log(solve(N, M, K, houses));
 }
